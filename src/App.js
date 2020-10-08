ddeee5cab31e7d445a0a5b65e26b65304a9c30ff
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-10-08 19:05:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
-      <DataFetchingOne/>
-      {/* <PostList/> */}
+    {/*  <DataFetchingOne/>*/}
+      <PostList/> 
     </div>
   );
 }
