6baba0900adbe53339b1e6e6a391feb122c4f8a6
ANSHUMAN YADAV <45606571+anshumanyadav254@users.noreply.github.com>
2020-10-09 13:23:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DataFetchingOne from './Components/DataFetching';
-import PostList from './Components/PostList';
+import FetchUser from './Components/FetchUser';
 
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
-    {/*  <DataFetchingOne/>*/}
-      <PostList/> 
+      <FetchUser/>
     </div>
   );
 }
